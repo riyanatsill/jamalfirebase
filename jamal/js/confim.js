aede17950f005e668f252ca2811873796e149372
aryaelmahdi <106580305+aryaelmahdi@users.noreply.github.com>
2023-06-14 11:54:09 +0700

    update test confirm

---
+++ jamal/js/confim.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function() {
+    var isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (isLoggedIn !== 'true') {
+        // Redirect to the login page if not logged in
+        location.assign("login.html");
+    }
+});
+
+var username = sessionStorage.getItem('username');
+var riotId = sessionStorage.getItem('riotId');
+var tagline = sessionStorage.getItem('tagline');
+var game = sessionStorage.getItem('game');
+var email = sessionStorage.getItem('email');
+var price = sessionStorage.getItem('price');
+var payment = sessionStorage.getItem('payment');
+
+var textNama = document.getElementById("textNama").value = riotId;
+var textTagline = document.getElementById("textTagline").value = tagline;
+var textEmail = document.getElementById("textEmail").value = email;
+var textPrice = document.getElementById("textPrice").value = price;
+var textPayment = document.getElementById("textPayment").value = payment;
+
+document.getElementById("sumbitForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (username) {
+        var uid = firebase.database().ref().child('Transaction').push().key;
+
+        var data = {
+            Id: riotId,
+            tagline: tagline,
+            game: game,
+            email: email,
+            selectedProduct: selectedProduct,
+            username: username,
+            selectedPayment: selectedPayment
+        }
+
+        var updates = {};
+        updates['/Transaction/' + uid] = data;
+        firebase.database().ref().update(updates);
+        console.log('Data stored to Firebase:', data);
+        location.assign("success.html");
+    } else {
+        console.log('User session not found. Please login first.');
+    }
+})
--- jamal/js/valorant.js
+++ jamal/js/valorant.js
@@ -1,25 +1,25 @@
 // Get all the radio buttons for products
 const productRadioButtons = document.querySelectorAll('input[name="price"]');
-                  
+
 let selectedProductDiv = null; // Keep track of the selected product div
 
 // Add event listener to each product radio button
-productRadioButtons.forEach(function (radio) {
-  radio.addEventListener('click', function () {
-    // Get the parent div of the radio button
-    const div = this.parentElement;
+productRadioButtons.forEach(function(radio) {
+    radio.addEventListener('click', function() {
+        // Get the parent div of the radio button
+        const div = this.parentElement;
 
-    // Restore the background color of the previously selected product div, if any
-    if (selectedProductDiv !== null && selectedProductDiv !== div) {
-      selectedProductDiv.style.backgroundColor = '#E8E8E8';
-    }
+        // Restore the background color of the previously selected product div, if any
+        if (selectedProductDiv !== null && selectedProductDiv !== div) {
+            selectedProductDiv.style.backgroundColor = '#E8E8E8';
+        }
 
-    // Change the background color of the current product div
-    if (this.checked) {
-      selectedProductDiv = div; // Store the current product div as the selected div
-      div.style.backgroundColor = 'rgb(139, 192, 253)'; // Change the color to your desired value
-    }
-  });
+        // Change the background color of the current product div
+        if (this.checked) {
+            selectedProductDiv = div; // Store the current product div as the selected div
+            div.style.backgroundColor = 'rgb(139, 192, 253)'; // Change the color to your desired value
+        }
+    });
 });
 
 // Get all the radio buttons for products
@@ -28,92 +28,100 @@ const paymentRadioButtons = document.querySelectorAll('input[name="payment"]');
 let selectedProductDiv2 = null; // Keep track of the selected product div
 
 // Add event listener to each product radio button
-paymentRadioButtons.forEach(function (radio) {
-  radio.addEventListener('click', function () {
-    // Get the parent div of the radio button
-    const div = this.parentElement;
+paymentRadioButtons.forEach(function(radio) {
+    radio.addEventListener('click', function() {
+        // Get the parent div of the radio button
+        const div = this.parentElement;
 
-    // Restore the background color of the previously selected product div, if any
-    if (selectedProductDiv2 !== null && selectedProductDiv2 !== div) {
-      selectedProductDiv2.style.backgroundColor = '#E8E8E8';
-    }
+        // Restore the background color of the previously selected product div, if any
+        if (selectedProductDiv2 !== null && selectedProductDiv2 !== div) {
+            selectedProductDiv2.style.backgroundColor = '#E8E8E8';
+        }
 
-    // Change the background color of the current product div
-    if (this.checked) {
-      selectedProductDiv2 = div; // Store the current product div as the selected div
-      div.style.backgroundColor = 'rgb(139, 192, 253)'; // Change the color to your desired value
-    }
-  });
+        // Change the background color of the current product div
+        if (this.checked) {
+            selectedProductDiv2 = div; // Store the current product div as the selected div
+            div.style.backgroundColor = 'rgb(139, 192, 253)'; // Change the color to your desired value
+        }
+    });
 });
 
 
 var databaseRef = firebase.database().ref('Transaction/');
-		    let fix = "false";
-		
-		    document.getElementById('registerForm').addEventListener('submit', function(event) {
-		            event.preventDefault(); // Prevent form submission
-                    const riotId = document.getElementById('nama').value.trim();
-                    const tagline = document.getElementById('uid').value.trim();
-                    const email = document.getElementById('gmail').value.trim();
+let fix = "false";
 
-                    // Retrieve selected radio button values
-                    const selectedPrice = document.querySelector('input[name="price"]:checked');
-                    const selectedPayment = document.querySelector('input[name="payment"]:checked');
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    const riotId = document.getElementById('nama').value.trim();
+    const tagline = document.getElementById('uid').value.trim();
+    const email = document.getElementById('gmail').value.trim();
 
-                    if (riotId === '' || tagline === '' || email === '' || !selectedPrice || !selectedPayment) {
-                        // If any of the required fields or radio buttons are empty, display an error message
-                        alert('Please fill in all the required fields and make a selection for both price and payment.');
-                    } else {
-                        // Call the save_valorant() function
-                        save_valorant();
+    // Retrieve selected radio button values
+    const selectedPrice = document.querySelector('input[name="price"]:checked');
+    const selectedPayment = document.querySelector('input[name="payment"]:checked');
 
-                        // Redirect to the next page
-                        alert('Successfully');
-                        location.assign("success.html");
-                        console.log('Data stored to Firebase:', riotId, tagline, email);
-                        console.log('Selected Price:', selectedPrice.value);
-                        console.log('Selected Payment:', selectedPayment.value);
-                    }
-		            
-		        });
-		
-		    function save_valorant(){
-                var Id = document.getElementById('nama').value;
-                var tagline = document.getElementById('uid').value;
-                var email = document.getElementById('gmail').value;
-                var game = document.getElementById('game').value;
-                var selectedProduct = document.querySelector('input[name="price"]:checked').value;
-                var selectedPayment = document.querySelector('input[name="payment"]:checked').value;
-                var username = sessionStorage.getItem('username');
+    if (riotId === '' || tagline === '' || email === '' || !selectedPrice || !selectedPayment) {
+        // If any of the required fields or radio buttons are empty, display an error message
+        alert('Please fill in all the required fields and make a selection for both price and payment.');
+    } else {
+        // Call the save_valorant() function
+        save_valorant();
 
-            
-            if (username) {
-            var uid = firebase.database().ref().child('Transaction').push().key;
-            
-            var data = {
-                Id: Id,
-                tagline: tagline,
-                game: game,
-                email: email,
-                selectedProduct: selectedProduct,
-                username: username,
-                selectedPayment: selectedPayment
-            }
+        // Redirect to the next page
+        alert('Successfully');
+        //
+        location.assign("success.html");
+        console.log('Data stored to Firebase:', riotId, tagline, email);
+        console.log('Selected Price:', selectedPrice.value);
+        console.log('Selected Payment:', selectedPayment.value);
+    }
 
-            var updates = {};
-            updates['/Transaction/' + uid] = data;
-            firebase.database().ref().update(updates);
-            console.log('Data stored to Firebase:', data);
-            }else {
-            console.log('User session not found. Please login first.');
-            }
-        }
+});
 
-        // Check if the user is logged in when the page loads
-      window.addEventListener('load', function() {
-        var isLoggedIn = sessionStorage.getItem('isLoggedIn');
-        if (isLoggedIn !== 'true') {
-          // Redirect to the login page if not logged in
-          location.assign("login.html");
-        }
-      });
+function save_valorant() {
+    var Id = document.getElementById('nama').value;
+    var tagline = document.getElementById('uid').value;
+    var email = document.getElementById('gmail').value;
+    var game = document.getElementById('game').value;
+    var selectedProduct = document.querySelector('input[name="price"]:checked').value;
+    var selectedPayment = document.querySelector('input[name="payment"]:checked').value;
+    var username = sessionStorage.getItem('username');
+
+
+    if (username) {
+        // var uid = firebase.database().ref().child('Transaction').push().key;
+
+        // var data = {
+        //     Id: Id,
+        //     tagline: tagline,
+        //     game: game,
+        //     email: email,
+        //     selectedProduct: selectedProduct,
+        //     username: username,
+        //     selectedPayment: selectedPayment
+        // }
+
+        // var updates = {};
+        // updates['/Transaction/' + uid] = data;
+        // firebase.database().ref().update(updates);
+        // console.log('Data stored to Firebase:', data);
+
+        sessionStorage.setItem('riotId', Id);
+        sessionStorage.setItem('tagline', tagline);
+        sessionStorage.setItem('tagline', game);
+        sessionStorage.setItem('email', email);
+        sessionStorage.setItem('price', selectedProduct);
+        sessionStorage.setItem('payment', selectedPayment);
+    } else {
+        console.log('User session not found. Please login first.');
+    }
+}
+
+// Check if the user is logged in when the page loads
+window.addEventListener('load', function() {
+    var isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (isLoggedIn !== 'true') {
+        // Redirect to the login page if not logged in
+        location.assign("login.html");
+    }
+});
